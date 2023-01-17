f3655da26e676085a883f663dee722d0776bddf0
Phen19 <phenriquelgsilva@gmail.com>
2023-01-17 15:14:40 -0300

    Exercicio olá spring

---
+++ src/main/java/com/exercicio/demo/controller/OlaController.java
@@ -0,0 +1,15 @@
+package com.exercicio.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ola")
+public class OlaController {
+    
+    @GetMapping
+    public String olaSpring(){
+        return "Olá Spring!!!";
+    }
+}
